8f1e798788c5a483319b569664b2d2f102406f3f
hanbinseo <shb204@gmail.com>
2021-03-30 16:21:37 +0900

    inner class with this

--- app/src/main/java/android/studio/practice/buttonwithinnerclass/MainActivity.java
+++ app/src/main/java/android/studio/practice/buttonwithinnerclass/MainActivity.java
@@ -3,6 +3,7 @@ package android.studio.practice.buttonwithinnerclass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,17 @@ public class MainActivity extends AppCompatActivity {
         mTextView1 = findViewById(R.id.textView1);
 
         mButton1.setOnClickListener(new MyOnclickListener(this));
+    }
+
+    private class MyOnclickListener implements View.OnClickListener {
+        MainActivity mainActivity;
+        public MyOnclickListener(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mainActivity.mTextView1.setText("You clicked Hanbin's button!");
+        }
     }
 }
